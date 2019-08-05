423bd267f1a58e40df682b44d0207776b39cd733
mitroulias77 <dm.mitroulias@gmail.com>
2019-08-05 10:11:49 +0300

    Scraping

--- scripts/scrape_nsk.py
+++ scripts/scrape_nsk.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup as soup  # parse html text
+from cffi.backend_ctypes import xrange
 from lxml import html
 from warnings import warn
+###########################################################
 #Εδώ γίνεται το request της σελίδας του νομικού συμβουλίου
+
 nsk_url = 'http://www.nsk.gr/web/nsk/anazitisi-gnomodoteseon?p_p_id=nskconsulatories_WAR_nskplatformportlet&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-4&p_p_col_pos=2&p_p_col_count=3'
 post_data = {"_nskconsulatories_WAR_nskplatformportlet_isSearch": "1",
-             "_nskconsulatories_WAR_nskplatformportlet_inputDatefrom": 2019,
+             "_nskconsulatories_WAR_nskplatformportlet_inputDatefrom": 2000,
              "_nskconsulatories_WAR_nskplatformportlet_consulState": "1"}
+
+
 response = requests.post(nsk_url , post_data)
+##########################################################
+
 ##########################################################
 #parsing και scraping της σελίδας
 if response.status_code != 200:
@@ -37,7 +44,9 @@ for item in decisions:
 for char in decisions[:]:
     if char in punctuation:
         decisions.remove(char)
+###############################################################
 
+#DATA-FRAME και export σε xlsx
 df = pd.DataFrame({'Title':concultatories,
                    'Concultatory':decisions,
                    'Status':"1"})
