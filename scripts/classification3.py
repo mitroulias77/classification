987599a45bf34dd70b9176ef03254f1675c368e0
mitroulias77 <dm.mitroulias@gmail.com>
2019-08-09 00:54:39 +0300

    utils 3

--- scripts/classification3.py
+++ scripts/classification3.py
@@ -1,32 +1,18 @@
 '''
-https://medium.com/@hjhuney/implementing-a-random-forest-classification-model-in-python-583891c99652
-
+https://stackabuse.com/text-classification-with-python-and-scikit-learn/
 ΥΛΟΠΟΙΗΣΗ RANDOM FOREST KATΗΓΟΡΙΟΠΟΙΗΤΗ
 '''
-
 import re
-import json
 from os import path
-import nltk
 import pandas as pd
-from pandas import DataFrame,np
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_selection import chi2
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
-from sklearn.metrics import confusion_matrix
-from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import LinearSVC
-from sklearn.model_selection import cross_val_score
-import seaborn as sns
-from IPython.display import display
 from nltk.corpus import stopwords
 import greek_stemmer as gr_stemm
 from classification.utils import remove_emphasis
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
 
 file = path.join('data', 'nsk_scrape.xlsx')
 xl = pd.ExcelFile(file)
@@ -37,90 +23,78 @@ corpus = []
 STOPWORDS = set(stopwords.words('greek'))
 
 print(df.shape[0])
+from nltk.stem import WordNetLemmatizer
 
+stemmer = gr_stemm.GreekStemmer()
+lemmetizer = WordNetLemmatizer()
 for i in range(0, df.shape[0]):
-    subject = re.sub(r"\d+", '', df['Concultatory'][i],flags=re.I)
-    subject = re.sub(r"[-,()/@\'?\.$%_+\d]", '', df['Concultatory'][i],flags=re.I)
-    stemmer = gr_stemm.GreekStemmer()
+    # Remove all the special characters
+    subject = re.sub(r'\W', ' ', df['Concultatory'][i],flags=re.I)
+    # remove all single characters
+    subject = re.sub(r'\s+[a-zA-Z]\s+', ' ', subject)
+    # Remove single characters from the start
+    subject = re.sub(r'\^[a-zA-Z]\s+', ' ' , subject)
+    # Substituting multiple spaces with single space
+    subject = re.sub(r'\s+',' ', subject)
+    # Removing prefixed 'b'
+    subject = re.sub(r'^b\s+' , '' , subject)
+    # Lemmatization
     subject = subject.split()
+
     subject = [remove_emphasis(x) for x in subject]
     subject = [x.upper() for x in subject]
     subject = [stemmer.stem(word) for word in subject if not word in STOPWORDS and len(word)>=3]
-    subject = [x.lower() for x in subject]
+    subject = [lemmetizer.lemmatize(word) for word in subject]
     subject = " ".join(subject)
+    subject = subject.lower()
     corpus.append(subject)
 
-X_stem = pd.DataFrame(corpus, columns=['Concultatory'])
+X = pd.DataFrame(corpus, columns=['Concultatory'])
 
-X_stem.head()
+X.head()
 
-result = X_stem.join(df[['Title']['Year']['Status']])
-result.groupby(['Status']).size()
-result.head()
-result.columns = ['Concultatory','Title','Year','Status']
-tfidf = TfidfVectorizer()
-tfidf.fit(result['Concultatory'])
-
-X = tfidf.transform(result['Concultatory'])
+'''vectorizer = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=STOPWORDS)
+X = vectorizer.fit_transform(X['Concultatory']).toarray()
+'''
 y = df['Status']
 
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidfconverter = TfidfVectorizer(max_features=2500, min_df=15, max_df=0.7, stop_words=STOPWORDS)
+X = tfidfconverter.fit_transform(X['Concultatory']).toarray()
+
 from sklearn.model_selection import train_test_split
 # Δημιουργία train και test συνόλου
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=66)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 #Δημιουργία μοντέλου Random Forest
 from sklearn import model_selection
-rfc = RandomForestClassifier()
-rfc.fit(X_train,y_train)
+classifier = RandomForestClassifier(n_estimators=1000, random_state=0)
+classifier.fit(X_train, y_train)
 # Προβλέψεις κατηγοριοποιητή
-rfc_predict = rfc.predict(X_test)
+y_pred = classifier.predict(X_test)
 
 #Αξιολογηση του μοντέλου
 
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-rfc_cv_score = cross_val_score(rfc, X, y, cv=10, scoring='roc_auc')
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
+print(accuracy_score(y_test, y_pred))
 
-#Tuning Hyperparameters
-from sklearn.model_selection import RandomizedSearchCV
-# number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
-# number of features at every split
-max_features = ['auto', 'sqrt']
+#Saving and Loading the Model
+with open('data/text_classifier', 'wb') as picklefile:
+    pickle.dump(classifier,picklefile)
+#load the model
+with open('data/text_classifier', 'rb') as training_model:
+    model = pickle.load(training_model)
+'''
+ Let's predict the sentiment for the test set using our loaded model and 
+ see if we can get the same results. 
+'''
+y_pred2 = model.predict(X_test)
 
-# max depth
-max_depth = [int(x) for x in np.linspace(100, 500, num = 11)]
-max_depth.append(None)
-# create random grid
-random_grid = {
- 'n_estimators': n_estimators,
- 'max_features': max_features,
- 'max_depth': max_depth
- }
-# Random search of parameters
-rfc_random = RandomizedSearchCV(estimator = rfc, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
-# Fit the model
-rfc_random.fit(X_train, y_train)
-# print results
-print(rfc_random.best_params_)
-
-
-rfc = RandomForestClassifier(n_estimators=1200, max_depth=300, max_features='sqrt')
-rfc.fit(X_train,y_train)
-rfc_predict = rfc.predict(X_test)
-rfc_cv_score = cross_val_score(rfc, X, y, cv=10, scoring='roc_auc')
-print("=== Confusion Matrix ===")
-print(confusion_matrix(y_test, rfc_predict))
-print('\n')
-print("=== Classification Report ===")
-print(classification_report(y_test, rfc_predict))
-print('\n')
-print("=== All AUC Scores ===")
-print(rfc_cv_score)
-print('\n')
-print("=== Mean AUC Score ===")
-print("Mean AUC Score - Random Forest: ", rfc_cv_score.mean())
-
-
+print(confusion_matrix(y_test, y_pred2))
+print(classification_report(y_test, y_pred2))
+print(accuracy_score(y_test, y_pred2))
