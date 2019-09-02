37c02fab5fea52c31d01b8e20cbcf47a4c1b22dc
mitroulias <mitroulias@cti.gr>
2019-09-02 17:04:38 +0300

    updates___NEWWWW

--- scripts/scrape_nsk.py
+++ scripts/scrape_nsk.py
@@ -6,17 +6,18 @@ from warnings import warn
 import os
 
 ###########################################################
-from scripts.data_list import lawyer_list , provision_list
+from classification.data_list import provision_list, lawyer_list
 
-columns=['Title','Concultatory','Status','Year','Category']
+columns=['Title','Concultatory','Category']
 
 '''
 0:μη αποδεκτή, 1:αποδεκτή,2:εν μερει αποδεκτή, 3:ανακληθηκε το ερώτημα,
 4:Για την αποδοχή ή μη επικοινωνήστε με τον Σχ. Επιστ. Δραστηριοτήτων κ Δημοσίων Σχέσεων, -1: Εκκρεμεί η Αποδοχή
 '''
 df_all = pd.DataFrame(columns=columns)
-for status in ['-1','1']:
-    for year in range(2001, 2015):
+
+for status in ['1','-1']:
+    for year in range(2000, 2020):
         nsk_url = 'http://www.nsk.gr/web/nsk/anazitisi-gnomodoteseon?p_p_' \
                   'id=nskconsulatories_WAR_nskplatformportlet&p_p_lifecycle=0&p_p_state=normal&p_p_' \
                   'mode=view&p_p_col_id=column-4&p_p_col_pos=2&p_p_col_count=3'
@@ -55,15 +56,12 @@ for status in ['-1','1']:
                 decisions.remove(char)
 
         # Εξαγωγή κατηγοριών από τα λήμματα-keywords
-
         #keywords = articles.xpath('//*[@id="resdiv"]/div/div[1]/div[2]/p[2]/strong[1]')
         keywords = articles.xpath('//div[@class="article_text"]/p/text()')
         #keywords[3].strip().split (',')
 
         keywords = [x.strip() for x in keywords]
         keywords = [x for x in keywords if len(x) > 0]
-        #keywords = [x for x in keywords if '/' not in x]
-        #keywords = [x for x in keywords if not any(c.isdigit() for c in x)]
         keywords = [x for x in keywords if 'Εκκρεμεί αποδοχή' not in x]
         keywords = [x for x in keywords if 'Αποδεκτή' not in x]
         keywords = [x for x in keywords if x not in lawyer_list]
@@ -87,18 +85,26 @@ for status in ['-1','1']:
                 keywords.remove(chr)
 
         keywords_list = [x.split(',') for x in keywords]
+        #keywords_list = [w.replace (',', ' |') for w in keywords]
+        s1 = pd.Series(concultatories, name='Title')
+        s2 = pd.Series(decisions, name='Concultatory')
+        s3 = pd.Series(keywords_list, name='Category')
 
+        df = pd.concat([s1,s2,s3], axis=1)
         #DATA-FRAME και export σε xlsx
-        df = pd.DataFrame({'Title':concultatories,
+        '''
+        df_nsk = pd.DataFrame({'Title':concultatories,
                            'Concultatory':decisions,
                            'Status':status,
                            'Year':year,
                            'Category':keywords
                           })
-        df_all = pd.concat([df_all, df], ignore_index=True,sort=True)
-    #αφαιρεση κενών
+        '''
+        df_all = pd.concat([df_all, df],ignore_index=True,sort=True)
+
+    #αφαιρεση τυχόν κενών στους τίτλους και στις αποφάσεις των γνωμοδοτήσεων
     df_all['Title'] = df_all['Title'].apply(lambda x: x.rstrip())
     df_all['Concultatory'] = df_all['Concultatory'].apply(lambda x: x.rstrip())
     #Αποθήκευση df σε xlsx
-    fname_path = os.path.join('data','nsk_scrape.xlsx')
+    fname_path = os.path.join('data','nsk_multilabel.xlsx')
     df_all.to_excel(fname_path, index=False)
